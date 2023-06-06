6be8377a56dc1ed9e44ee0999041776f5041b450
Justas Lapiene <justas63@gmail.com>
2023-06-06 17:32:04 +0200

    created the main file structure. added header, footer

---
+++ src/Components/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <div className="footer bg-dark d-flex justify-content-center py-1">Copyright @Justas Lapiene </div>
+}
---
+++ src/Components/header.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return <div className="header w-100 bg-dark d-flex align-items-center justify-content-between px-1">
+        <h1 className="p-10">Let's Chat</h1>
+        <button className="btn btn-primary">Login</button>
+    </div>
+}
